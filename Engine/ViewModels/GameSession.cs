c71287629061cbdbccd69c97d14675b6610caa1b
niin44 <niin44@DESKTOP>
2018-07-19 21:45:47 +0200

    Creating the GameSession (ViewModel) Class

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+namespace Engine.ViewModels
+{
+    class GameSession
+    {
+        Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Models.Player();
+            CurrentPlayer.Name = "Scott";
+            CurrentPlayer.Gold = 1000000;
+        }
+    }
+}
