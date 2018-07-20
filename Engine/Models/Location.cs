f20a591324ed0ced7ff79435cb712009496efffc
niin44 <niin44@DESKTOP>
2018-07-20 16:21:42 +0200

    Creating the Location class

---
+++ Engine/Models/Location.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class Location
+    {
+        public int XCordinate { get; set; }
+        public int YCodrinate { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageName { get; set; }
+    }
+}
--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -6,15 +6,27 @@ using System.Threading.Tasks;
 using Engine.Models;
 namespace Engine.ViewModels
 {
-    class GameSession
+    public class GameSession
     {
-        Player CurrentPlayer { get; set; }
+        public Player CurrentPlayer { get; set; }
+        public Location CurrentLocation { get; set; }
 
         public GameSession()
         {
-            CurrentPlayer = new Models.Player();
+            CurrentPlayer = new Player();
             CurrentPlayer.Name = "Scott";
+            CurrentPlayer.CharacterClass = "Fighter";
+            CurrentPlayer.HitPoints = 10;
             CurrentPlayer.Gold = 1000000;
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.Level = 1;
+
+            CurrentLocation = new Location();
+            CurrentLocation.Name = "Home";
+            CurrentLocation.XCordinate = 0;
+            CurrentLocation.YCodrinate = -1;
+            CurrentLocation.Description = "This is your house.";
+
         }
     }
 }
